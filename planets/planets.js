4b3268094b14cfb2d40e1b4c67b589935c31998e
Your Name <you@example.comgit config --global user.email you@example.com>
2021-01-11 20:57:53 +0200

    curs 3 + 4

---
+++ planets/planets.js
@@ -0,0 +1,26 @@
+let button = document.getElementById("check")
+button.addEventListener("click", function () {
+
+    let planet = document.getElementById("redplanet")
+
+    planet.innerText = "Alien invasion"
+    planet.classList.add("alert")
+    // planet.style.color = "red"
+    // planet.remove()
+    // planet.style.fontWeight = "bold"
+})
+
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        let planet = document.getElementById("redplanet")
+
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+    })
+
+
+document.getElementById("greenplanet")
+.addEventListener("click", function () {
+    let planet = document.getElementById("greenplanet")
+    planet.classList.add("alert")
+})
