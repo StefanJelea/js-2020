1b8c9ad14ddce29f835914184ebedc2aa5b470a7
Your Name <you@example.comgit config --global user.email you@example.com>
2021-01-15 20:47:32 +0200

    curs 5 update

---
+++ array/array.js
@@ -0,0 +1,14 @@
+let bubbles = [60, 59, 48, 71, 45, 51, 65, 70, 49, 38]
+
+let fruits = ["Apple", "Ananas", "Mango", "Orange", "Cucumber", "Tomato"]
+
+bubbles.sort()
+max = bubbles.pop()
+
+
+fruits.sort()
+fruits.reverse()
+
+
+console.log(max)
+console.log(fruits)
