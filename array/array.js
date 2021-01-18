6fe41ab3c58818e0a1fe77f239412ac435e68dbd
Your Name <you@example.comgit config --global user.email you@example.com>
2021-01-18 20:47:06 +0200

    curs 6

--- array/array.js
+++ array/array.js
@@ -12,3 +12,29 @@ fruits.reverse()
 
 console.log(max)
 console.log(fruits)
+
+
+for (let index = 0; index < fruits.length; index++) {
+    const fruit = fruits[index];
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+
+}
+
+
+for (const fruit of fruits) {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+}
+fruits.forEach(fruit => {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+});
+for (const fruit in fruits) {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+}
--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,16 +1,22 @@
 var numberOfBubbles = 10
-for (let i = 0; i< numberOfBubbles; i++) {
+for (let i = 0; i < numberOfBubbles; i++) {
     newBubble()
 }
-function newBubble(){
+function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
     let x = randomNumber(100)
     let delay = randomNumber(1000)
+    let red = randomNumber(255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(3) + 1
     let bubbleSize = randomNumber(50)
-    bubble.style.left = x + "vw"
-    bubble.style.animationDelay = delay + "ms"
+    bubble.style.left = `${x}vw`
+    bubble.style.animationDelay = `${delay}ms`
     bubble.style.scale = bubbleSize + "em"
+    bubble.style.borderColor = `rgb(${red}, ${green}, ${blue})`
+    bubble.style.height = bubble.style.width = `${size}em`
     document.querySelector("body").appendChild(bubble)
 }
 function randomNumber(max) {
